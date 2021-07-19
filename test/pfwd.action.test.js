0e09fe0c9abc6777940d8b49bfb4fac1b4b57c33
Val Neekman <val@neekware.com>
2021-07-19 18:43:38 -0400

    clean up

--- test/pfwd.action.test.js
+++ test/pfwd.action.test.js
@@ -1,54 +1,62 @@
-const { BN, constants, expectEvent } = require('@openzeppelin/test-helpers');
-const PlayItForward = artifacts.require('PlayItForward');
+const { BN, constants, expectEvent } = require("@openzeppelin/test-helpers");
+const PlayItForward = artifacts.require("PlayItForward");
 
 contract("PlayItForward", (accounts) => {
-    const [deployer, recipient] = accounts;
-    const owner = deployer
+  const [deployer, recipient] = accounts;
+  const owner = deployer;
 
-    const name = 'PlayItForward';
-    const symbol = 'PFWD';
-    const decimals = 18;
-    const totalSupply = 1000000;
+  const name = "PlayItForward";
+  const symbol = "PFWD";
+  const decimals = 18;
+  const totalSupply = 1000000;
 
-    const toEth = (balance) => web3.utils.fromWei(balance, 'ether').toString();
-    const toWei = (balance) => web3.utils.toWei(balance, 'ether').toString();
+  const toEth = (balance) => web3.utils.fromWei(balance, "ether").toString();
+  const toWei = (balance) => web3.utils.toWei(balance, "ether").toString();
 
-    const { ZERO_ADDRESS } = constants;
+  const { ZERO_ADDRESS } = constants;
 
-    describe('Actions', function () {
-        before(async () => {
-            pfwd = await PlayItForward.deployed();
-        });
+  before(async () => {
+    pfwd = await PlayItForward.deployed();
+  });
 
-        it(`can transfer ${totalSupply/2} token to a recipient `, async () => {
-            const toTransfer = new BN(totalSupply/2);
-            await pfwd.transfer(recipient, toWei(toTransfer));
-            assert.equal(toEth(await pfwd.balanceOf(recipient)), `${totalSupply/2}`);
-        });
-
-        it(`can transfer all tokens to a recipient `, async () => {
-            const toTransfer = new BN(totalSupply/2);
-            await pfwd.transfer(recipient, toWei(toTransfer));
-            assert.equal(toEth(await pfwd.balanceOf(recipient)), `${totalSupply}`);
-        });
-
-        it(`cannot transfer more tokens that owner has `, async () => {
-            // owner has no tokens anymore
-            assert.equal(toEth(await pfwd.balanceOf(owner)), '0');
-
-            // we have spend all tokens so we can't transfer more, not even 1
-            const toTransfer = new BN(1);
-
-            try {
-                await pfwd.transfer(recipient, toWei(toTransfer)); 
-            }
-            catch (error) {
-                assert.equal(error.message.includes('transfer amount exceeds balance'), true);
-            }
-
-            // recipient's balance should not have changed
-            assert.equal(toEth(await pfwd.balanceOf(recipient)), `${totalSupply}`);
-        });
+  describe("Actions", function () {
+    before(async () => {
+      pfwd = await PlayItForward.deployed();
     });
-    
+
+    it(`can transfer ${totalSupply / 2} token to a recipient `, async () => {
+      const toTransfer = new BN(totalSupply / 2);
+      await pfwd.transfer(recipient, toWei(toTransfer));
+      assert.equal(
+        toEth(await pfwd.balanceOf(recipient)),
+        `${totalSupply / 2}`
+      );
+    });
+
+    it(`can transfer all tokens to a recipient `, async () => {
+      const toTransfer = new BN(totalSupply / 2);
+      await pfwd.transfer(recipient, toWei(toTransfer));
+      assert.equal(toEth(await pfwd.balanceOf(recipient)), `${totalSupply}`);
+    });
+
+    it(`cannot transfer more tokens that owner has `, async () => {
+      // owner has no tokens anymore
+      assert.equal(toEth(await pfwd.balanceOf(owner)), "0");
+
+      // we have spend all tokens so we can't transfer more, not even 1
+      const toTransfer = new BN(1);
+
+      try {
+        await pfwd.transfer(recipient, toWei(toTransfer));
+      } catch (error) {
+        assert.equal(
+          error.message.includes("transfer amount exceeds balance"),
+          true
+        );
+      }
+
+      // recipient's balance should not have changed
+      assert.equal(toEth(await pfwd.balanceOf(recipient)), `${totalSupply}`);
+    });
+  });
 });
--- test/pfwd.maintain.test.js
+++ test/pfwd.maintain.test.js
@@ -1,30 +1,35 @@
-const { BN, constants, expectEvent } = require('@openzeppelin/test-helpers');
-const PlayItForward = artifacts.require('PlayItForward');
+const { BN, constants, expectEvent } = require("@openzeppelin/test-helpers");
+const PlayItForward = artifacts.require("PlayItForward");
+const { expect } = require("chai");
 
 contract("PlayItForward", (accounts) => {
-    const [deployer, recipient] = accounts;
-    const owner = deployer
+  const [deployer, recipient] = accounts;
+  const owner = deployer;
 
-    const name = 'PlayItForward';
-    const symbol = 'PFWD';
-    const decimals = 18;
-    const totalSupply = 1000000;
+  const name = "PlayItForward";
+  const symbol = "PFWD";
+  const decimals = 18;
+  const totalSupply = 1000000;
 
-    const toEth = (balance) => web3.utils.fromWei(balance, 'ether').toString();
-    const toWei = (balance) => web3.utils.toWei(balance, 'ether').toString();
+  const toEth = (balance) => web3.utils.fromWei(balance, "ether").toString();
+  const toWei = (balance) => web3.utils.toWei(balance, "ether").toString();
 
-    const { ZERO_ADDRESS } = constants;
+  const { ZERO_ADDRESS } = constants;
 
-    describe('Maintenance', function () {
-        before(async () => {
-            pfwd = await PlayItForward.deployed();
-        });
-        
-        it('holders can burn their tokens', async function () {
-            console.log(await pfwd.balanceOf(owner))
-            const amount = new BN(totalSupply);
-            const receipt = await pfwd.burn(amount, { from: owner });
-            expectEvent(receipt, 'Transfer', { from: owner, to: ZERO_ADDRESS, value: amount });
-        });
+  before(async () => {
+    pfwd = await PlayItForward.deployed();
+  });
+
+  describe("Maintenance", function () {
+    it("holders can burn their tokens", async function () {
+      console.log(await pfwd.balanceOf(owner));
+      const amount = new BN(totalSupply);
+      const receipt = await pfwd.burn(amount, { from: owner });
+      expectEvent(receipt, "Transfer", {
+        from: owner,
+        to: ZERO_ADDRESS,
+        value: amount,
+      });
     });
+  });
 });
