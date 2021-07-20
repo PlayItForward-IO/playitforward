0756ce9e415787a12c82a260955c1d73e02e224f
Val Neekman <val@neekware.com>
2021-07-20 18:28:03 -0400

    add ropsten config

--- truffle-config.js
+++ truffle-config.js
@@ -37,28 +37,27 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
       host: '127.0.0.1', // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
       network_id: '*', // Any network (default: none)
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          numberOfAddresses: 2,
+          privateKeys: [
+            process.env.TEST_METAMASK_PRIVATE_KEY_1, // deployer account (owner)
+            process.env.TEST_METAMASK_PRIVATE_KEY_2, // receiver account (recipient)
+          ],
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.TEST_INFURA_KEY}`,
+        }),
+      network_id: 3, // ropsten id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     rinkeby: {
       provider: () =>
         new HDWalletProvider({
@@ -67,7 +66,7 @@ module.exports = {
             process.env.TEST_METAMASK_PRIVATE_KEY_1, // deployer account (owner)
             process.env.TEST_METAMASK_PRIVATE_KEY_2, // receiver account (recipient)
           ],
-          providerOrUrl: `https://rinkeby.infura.io/${process.env.TEST_INFURA_VERSION}/${process.env.TEST_INFURA_KEY}`,
+          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.TEST_INFURA_KEY}`,
         }),
       network_id: 4, // Rinkeby id
       gas: 5500000, // Rinkeby has a lower block limit than mainnet
@@ -75,12 +74,6 @@ module.exports = {
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -106,7 +99,7 @@ module.exports = {
 
   plugins: ['truffle-plugin-verify'],
   api_keys: {
-    etherscan: process.env.TEST_RINKEBY_ETHERSCAN_KEY,
+    etherscan: process.env.ETHERSCAN_KEY,
   },
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
   //
