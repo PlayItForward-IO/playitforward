1b352321ff5706224ec3772ec47b975d93089023
Val Neekman <val@neekware.com>
2021-07-20 19:53:31 -0400

    Support Ropsten,Rinkeby

--- test/pfwd.core.test.js
+++ test/pfwd.core.test.js
@@ -1,53 +1,40 @@
-const { BN, constants, expectEvent } = require("@openzeppelin/test-helpers");
-const PlayItForward = artifacts.require("PlayItForward");
+const util = require('./pfwd.util.js');
+const PlayItForward = artifacts.require('PlayItForward');
 
-contract("PlayItForward", (accounts) => {
+contract('PlayItForward Token', (accounts) => {
   const [deployer, recipient] = accounts;
   const owner = deployer;
-
-  const name = "PlayItForward";
-  const symbol = "PFWD";
-  const decimals = 18;
-  const totalSupply = 1000000;
-
-  const toEth = (balance) => web3.utils.fromWei(balance, "ether").toString();
-  const toWei = (balance) => web3.utils.toWei(balance, "ether").toString();
-
-  const { ZERO_ADDRESS } = constants;
 
   before(async () => {
     pfwd = await PlayItForward.deployed();
   });
 
-  describe("Core", function () {
-    it(`has ${name} as name`, async () => {
-      assert.equal(await pfwd.name(), name);
+  describe('Core', function () {
+    it(`has ${util.name} as name`, async () => {
+      assert.equal(await pfwd.name(), util.name);
     });
 
-    it(`has ${symbol} as symbol`, async () => {
-      assert.equal(await pfwd.symbol(), symbol);
+    it(`has ${util.symbol} as symbol`, async () => {
+      assert.equal(await pfwd.symbol(), util.symbol);
     });
 
-    it(`has ${decimals} decimals`, async () => {
-      assert.equal(await pfwd.decimals(), `${decimals}`);
+    it(`has ${util.decimals} decimals`, async () => {
+      assert.equal(await pfwd.decimals(), `${util.decimals}`);
     });
 
-    it(`has minted ${totalSupply} token on deploy`, async () => {
-      assert.equal(toEth(await pfwd.totalSupply()), `${totalSupply}`);
+    it(`has minted ${util.totalSupply} token on deploy`, async () => {
+      assert.equal(util.toEth(await pfwd.totalSupply()), `${util.totalSupply}`);
     });
 
-    it(`has assigned ${totalSupply} token to owner (deployer) `, async () => {
-      assert.equal(toEth(await pfwd.balanceOf(deployer)), `${totalSupply}`);
+    it(`has assigned ${util.totalSupply} token to deployer (owner)`, async () => {
+      assert.equal(util.toEth(await pfwd.balanceOf(deployer)), `${util.totalSupply}`);
     });
 
-    it("has made the total supply immutable (mint api removed)", async () => {
+    it('has made the total supply immutable (mint api removed)', async () => {
       try {
-        await pfwd.mint(recipient, `${totalSupply}`);
+        await pfwd.mint(recipient, `${util.totalSupply}`);
       } catch (error) {
-        assert.equal(
-          error.message.includes("pfwd.mint is not a function"),
-          true
-        );
+        assert.equal(error.message.includes('pfwd.mint is not a function'), true);
       }
     });
   });
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 
 /**
  * Use this file to configure your truffle project. It's seeded with some
@@ -20,7 +20,7 @@ require("dotenv").config();
  *
  */
 
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -37,28 +37,42 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
+      host: '127.0.0.1', // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
+      network_id: '*', // Any network (default: none)
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
+    mainnet: {
+      provider: () =>
+        new HDWalletProvider({
+          numberOfAddresses: 2,
+          privateKeys: [
+            process.env.PROD_PRIVATE_KEY_1, // deployer account (owner)
+          ],
+          providerOrUrl: `https://mainnet.infura.io/v3/${process.env.PROD_INFURA_KEY}`,
+        }),
+      network_id: 1,
+      gas: 5500000,
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          numberOfAddresses: 2,
+          privateKeys: [
+            process.env.TEST_METAMASK_PRIVATE_KEY_1, // deployer account (owner)
+            process.env.TEST_METAMASK_PRIVATE_KEY_2, // receiver account (recipient)
+          ],
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.TEST_INFURA_KEY}`,
+        }),
+      network_id: 3, // ropsten id
+      gas: 5500000, // ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     rinkeby: {
       provider: () =>
         new HDWalletProvider({
@@ -67,20 +81,14 @@ module.exports = {
             process.env.TEST_METAMASK_PRIVATE_KEY_1, // deployer account (owner)
             process.env.TEST_METAMASK_PRIVATE_KEY_2, // receiver account (recipient)
           ],
-          providerOrUrl: `https://rinkeby.infura.io/${process.env.INFURA_VERSION}/${process.env.INFURA_KEY}`,
+          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.TEST_INFURA_KEY}`,
         }),
-      network_id: 4, // Rinkeby id
-      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      network_id: 4, // rinkeby id
+      gas: 5500000, // rinkeby has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -91,18 +99,23 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.6", // Fetch exact version from solc-bin (default: truffle's version)
+      version: '^0.8.6', // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        // evmVersion: "byzantium",
+      },
     },
   },
 
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_KEY,
+  },
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
   //
   // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
